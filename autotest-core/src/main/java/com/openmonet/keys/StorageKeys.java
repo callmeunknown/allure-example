b715dfc5549691f9a4e3601a64d3d0a81ad5ac87
Anton Timoshenko <anton.timoshenko@quadcode.com>
2024-09-12 16:47:00 +0300

    Initial commit

---
+++ autotest-core/src/main/java/com/openmonet/keys/StorageKeys.java
@@ -0,0 +1,15 @@
+package com.openmonet.keys;
+
+/**
+ * Хранилище ключей используемых в коде
+ */
+public class StorageKeys {
+
+    public static final String API_URL = "_apiUrl";
+    public static final String WEB_URL = "_webUrl";
+
+    public static final String STAGE_ADMIN_EMAIL = "";
+    public static final String STAGE_SECRET_KEY = "";
+    public static final String PROD_TOKEN = "_prodToken";
+    public static final String ACCESS_TOKEN = "_accessToken";
+}
